633f5164d70ea44ee88eff1b86c6bdc9d1570435
aparna <aparna@bio-fa02son201x.local>
2019-06-05 18:02:38 -0500

    added documentation to Dataset and test_dataset

--- Dataset.py
+++ Dataset.py
@@ -3,10 +3,14 @@ import math
 import pandas as pd
 import numpy as np
 
+
+#This class handles our datasets. It is responsible for the data manipulation and conversion responsible for taking a
+#csv, turning it into a dataframe, and then turning those pandas dataframe entries into strings suitable for json
+# interpretation
 class Dataset(object):
 
     dict = ['']
-
+    #initialize our class with our file name and our dataframe
     def __init__(self, fileName):
         self.fileName = fileName
         try:
@@ -17,7 +21,7 @@ class Dataset(object):
 
         #self.clean_data()
 
-
+#clean our data of non-ascii characters. If our column is called 'name', take out the '- CE' it shouldn't be here
     def clean_data(self):
         self.df = self.df.fillna("")
         self.df = self.df.replace({r'[^\x00-\x7F]+': ''}, regex=True, inplace=True)
@@ -26,13 +30,16 @@ class Dataset(object):
             self.df = self.df['name'].str.replace(' - CE', '', regex=True)
 
 
-
+#return our self.dataframe
     def get_dataframe(self):
        return self.df
 
+#replace our dataframe with another
     def replace_dataframe(self, dataframe):
         self.df = dataframe
 
+
+#chunkify our dataframe into pieces small enough for the zendesk API to handle
     def create_hundred_entity_chunks(self):
         num_full_dfs = math.floor(len(self.df)/100)
         dfs = []
@@ -42,15 +49,18 @@ class Dataset(object):
         dfs.append(pd.DataFrame(self.df.iloc[(i+1)*100:]))
         return dfs
 
+#if entries are the exact same, delete one
     def delete_identical_entries(self):
        pass
 
+#if entries are the same through name, email, and organization, and group, merge them
     def merge_similar_entries(self):
         pass
 
+#check to see if two entries are similar enough to merge
     def is_it_similar_enough(self):
         pass
-
+#take a pandas row and convert it into a string that we can pass to our ZendeskConnector class to upload to our database
     def to_json_format(self, df_entry, dataset):
         if dataset == 'organization':
             data = {"name":df_entry[0], "domain_names":df_entry[1], "details":df_entry[2], "notes":df_entry[3], "tags":df_entry[5], "organization_fields":{"region": df_entry[4]}}
@@ -78,12 +88,13 @@ class Dataset(object):
             return data
         return ""
 
+#return a boolean basod on if our custom field is null
     def custom_field_is_null(self,string):
         if string == "":
             return True
 
         return False
 
-
+#create a ZendeskConnector instance for an upload event -- this will use the create_many feature of the Zendesk API
     def upload(self):
         pass
--- test_dataset.py
+++ test_dataset.py
@@ -6,21 +6,23 @@ import pandas as pd
 
 from AcmeData.Dataset import Dataset
 
-
+#Tests for our Dataset class
 class TestDataset(unittest.TestCase):
 
+#set up instances for our testing
     def setUp(self):
         self.orgs = Dataset('organizations.csv')
         self.users = Dataset('users.csv')
         self.tickets = Dataset('tickets.csv')
 
+#tear down our instances when a test is complete
     def tearDown(self):
         self.orgs = None
         self.users = None
         self.tickets = None
 
 
-
+#test that our files are opening correctly and are stored where we think they are
     def test_open_file_base_case(self):
         #confirm the datasets are being loaded in correctly through pandas by checking the size
 
@@ -28,6 +30,7 @@ class TestDataset(unittest.TestCase):
 
         self.assertEqual(self.orgs.df.size, df.size)
 
+#if the file isn't found, let us know
     def test_if_file_not_found_throw_exception(self):
 
         organization = 'organization.csv'
@@ -36,6 +39,7 @@ class TestDataset(unittest.TestCase):
 
         self.assertTrue(set.df == "")
 
+#base case for cleaning our dataframe entries of ascii characters, NaNs and extra wordage in the name column
     def test_clean_data_base_case(self):
         pass
 
@@ -48,6 +52,7 @@ class TestDataset(unittest.TestCase):
     def remove_non_ascii_symbols_during_cleaning(self):
         pass
 
+#replace our dataframe within our instance with a new dataframe
     def test_replace_dataframe(self):
         set = Dataset('organizations.csv')
 
@@ -55,6 +60,7 @@ class TestDataset(unittest.TestCase):
 
         self.assertEqual(self.users.df.size, set.df.size, msg='our dfs need to be the same size')
 
+#make sure our dataframe is being broken up correctly and that no pieces are lost
     def test_create_hundred_entity_chunks_base(self):
         df = pd.read_csv('organizations.csv')
         num_dfs = math.ceil(len(df.index)/100)
@@ -63,6 +69,7 @@ class TestDataset(unittest.TestCase):
 
         self.assertEqual(num_dfs,len(df_array))
 
+#when our database is bigger than 5 sets of 100, ensure we aren't running into index out of bound errors
     def test_create_hundred_entity_chunks_bigger_sets(self):
 
         df = pd.read_csv('tickets.csv')
@@ -72,6 +79,8 @@ class TestDataset(unittest.TestCase):
 
         self.assertEqual(num_dfs, len(df_array))
 
+#test that our json format is showing up correctly for organizations - we are comparing the string created to a string
+#that was previously put into a create instance on Zendesk
     def test_to_json_format_for_orgs(self):
         string_should_be = {"name": "1,000 Bulbs", "domain_names": ['1000bulbs.com'], "details": "", "notes": "", "organization_fields":{"region": "apac"}, "tags": ['']}
         #there is an intentional mismatch between "tags":[''] and "tags":[""] to ensure they would still mean the same
@@ -81,7 +90,8 @@ class TestDataset(unittest.TestCase):
         string_is = self.orgs.to_json_format(val, 'organization')
 
         self.assertEqual(string_is, string_should_be)
-
+    
+#if we have arrays of values with len > 1, ensure all of our values are present
     def test_to_json_format_for_orgs_with_multiple_array_values(self):
         string_should_be = {"name": "1,000 Bulbs", "domain_names": ['1000bulbs.com', 'protonmail.com'], "details": "", "notes": "", "tags": ['vip'], "organization_fields":{"region": "apac"}}
 
@@ -90,6 +100,7 @@ class TestDataset(unittest.TestCase):
         string_is = self.orgs.to_json_format(val, 'organization')
         self.assertEqual(string_is,string_should_be)
 
+#if we have null values within our custom field, do not add these to our json string - they will cause an error
     def test_to_json_format_orgs_with_no_org_fields(self):
         string_should_be = {"name": "1,000 Bulbs", "domain_names": ['1000bulbs.com', 'protonmail.com'], "details": "", "notes": "", "tags": ['vip']}
 
@@ -100,6 +111,8 @@ class TestDataset(unittest.TestCase):
 
         self.assertEqual(string_is, string_should_be)
 
+#test that our json format is showing up correctly for users - we are comparing the string created to a string
+#that was previously put into a create instance on Zendesk
 
     def test_to_json_format_users(self):
         string_should_be = {
@@ -126,12 +139,27 @@ class TestDataset(unittest.TestCase):
 
         self.assertEqual(string_is, string_should_be)
 
+#test that our json format is showing up correctly for tickets - we are comparing the string created to a string
+#that was previously put into a create instance on Zendesk
     def test_to_json_format_tickets(self):
         pass
 
 
-    def test_upload(self):
+#test that our uploads are occuring correctly using a confirmation string
+    def test_upload_org(self):
         pass
 
+    def test_upload_org_create_many(self):
+        pass
+    def test_upload_user(self):
+        pass
 
+    def test_upload_org_create_many(self):
+        pass
+
+    def test_upload_ticket(self):
+        pass
+
+    def test_upload_org_create_many(self):
+        pass
 
